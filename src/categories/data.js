171e66e2c0afba41a9c1cada8dc772d77b9fcd5a
Maria-Aidarus <maidarus@andrew.cmu.edu>
2023-08-28 21:18:11 +0300

    Passed the lint and test tests

--- src/categories/data.js
+++ src/categories/data.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -27,108 +18,92 @@ const intFields = [
     'minTags', 'maxTags', 'postQueue', 'subCategoriesPerPage',
 ];
 module.exports = function (Categories) {
-    Categories.getCategoriesFields = function (cids, fields) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (!Array.isArray(cids) || !cids.length) {
-                return [];
-            }
-            const keys = cids.map(cid => `category:${cid}`);
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const categories = yield database_1.default.getObjects(keys, fields);
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const result = yield plugins_1.default.hooks.fire('filter:category.getFields', {
-                cids: cids,
-                categories: categories,
-                fields: fields,
-                keys: keys,
-            });
-            function defaultIntField(category, fields, fieldName, defaultField) {
-                if (!fields.length || fields.includes(fieldName)) {
-                    const useDefault = !category.hasOwnProperty(fieldName) ||
-                        category[fieldName] === null ||
-                        category[fieldName] === '' ||
-                        !utils_1.default.isNumber(category[fieldName]);
-                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call
-                    category[fieldName] = useDefault ? meta_1.default.config[defaultField] : category[fieldName];
-                }
-            }
-            function modifyCategory(category, fields) {
-                if (!category) {
-                    return;
-                }
-                defaultIntField(category, fields, 'minTags', 'minimumTagsPerTopic');
-                defaultIntField(category, fields, 'maxTags', 'maximumTagsPerTopic');
-                defaultIntField(category, fields, 'postQueue', 'postQueue');
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                database_1.default.parseIntFields(category, intFields, fields);
-                const escapeFields = ['name', 'color', 'bgColor', 'backgroundImage', 'imageClass', 'class', 'link'];
-                escapeFields.forEach((field) => {
-                    if (category.hasOwnProperty(field)) {
-                        category[field] = escapeHtml(String(category[field] || ''));
-                    }
-                });
-                if (category.hasOwnProperty('icon')) {
-                    category.icon = category.icon || 'hidden';
-                }
-                if (category.hasOwnProperty('post_count')) {
-                    category.totalPostCount = category.post_count;
-                }
-                if (category.hasOwnProperty('topic_count')) {
-                    category.totalTopicCount = category.topic_count;
-                }
-                if (category.description) {
-                    category.description = escapeHtml(String(category.description));
-                    category.descriptionParsed = category.descriptionParsed || category.description;
-                }
-            }
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const modifiedCategories = result.categories.map((category) => {
-                modifyCategory(category, fields);
-                return category;
-            });
-            return modifiedCategories;
+    Categories.getCategoriesFields = async function (cids, fields) {
+        if (!Array.isArray(cids) || !cids.length) {
+            return [];
+        }
+        const keys = cids.map(cid => `category:${cid}`);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const categories = await database_1.default.getObjects(keys, fields);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const result = await plugins_1.default.hooks.fire('filter:category.getFields', {
+            cids: cids,
+            categories: categories,
+            fields: fields,
+            keys: keys,
         });
+        function defaultIntField(category, fields, fieldName, defaultField) {
+            if (!fields.length || fields.includes(fieldName)) {
+                const useDefault = !category.hasOwnProperty(fieldName) ||
+                    category[fieldName] === null ||
+                    category[fieldName] === '' ||
+                    !utils_1.default.isNumber(category[fieldName]);
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call
+                category[fieldName] = useDefault ? meta_1.default.config[defaultField] : category[fieldName];
+            }
+        }
+        function modifyCategory(category, fields) {
+            if (!category) {
+                return;
+            }
+            defaultIntField(category, fields, 'minTags', 'minimumTagsPerTopic');
+            defaultIntField(category, fields, 'maxTags', 'maximumTagsPerTopic');
+            defaultIntField(category, fields, 'postQueue', 'postQueue');
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            database_1.default.parseIntFields(category, intFields, fields);
+            const escapeFields = ['name', 'color', 'bgColor', 'backgroundImage', 'imageClass', 'class', 'link'];
+            escapeFields.forEach((field) => {
+                if (category.hasOwnProperty(field)) {
+                    category[field] = escapeHtml(String(category[field] || ''));
+                }
+            });
+            if (category.hasOwnProperty('icon')) {
+                category.icon = category.icon || 'hidden';
+            }
+            if (category.hasOwnProperty('post_count')) {
+                category.totalPostCount = category.post_count;
+            }
+            if (category.hasOwnProperty('topic_count')) {
+                category.totalTopicCount = category.topic_count;
+            }
+            if (category.description) {
+                category.description = escapeHtml(String(category.description));
+                category.descriptionParsed = category.descriptionParsed || category.description;
+            }
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const modifiedCategories = result.categories.map((category) => {
+            modifyCategory(category, fields);
+            return category;
+        });
+        return modifiedCategories;
     };
     // functions within the Category interface
-    Categories.getCategoryData = function (cid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const categories = yield Categories.getCategoriesFields([cid], []);
-            return categories && categories.length ? categories[0] : null;
-        });
+    Categories.getCategoryData = async function (cid) {
+        const categories = await Categories.getCategoriesFields([cid], []);
+        return categories && categories.length ? categories[0] : null;
     };
-    Categories.getCategoriesData = function (cids) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield Categories.getCategoriesFields(cids, []);
-        });
+    Categories.getCategoriesData = async function (cids) {
+        return await Categories.getCategoriesFields(cids, []);
     };
-    Categories.getCategoryField = function (cid, field) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const category = yield Categories.getCategoryFields(cid, [field]);
-            return category ? category[field] : null;
-        });
+    Categories.getCategoryField = async function (cid, field) {
+        const category = await Categories.getCategoryFields(cid, [field]);
+        return category ? category[field] : null;
     };
-    Categories.getCategoryFields = function (cid, fields) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const categories = yield Categories.getCategoriesFields([cid], fields);
-            return categories ? categories[0] : null;
-        });
+    Categories.getCategoryFields = async function (cid, fields) {
+        const categories = await Categories.getCategoriesFields([cid], fields);
+        return categories ? categories[0] : null;
     };
-    Categories.getAllCategoryFields = function (fields) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const cids = yield Categories.getAllCidsFromSet('categories:cid');
-            return yield Categories.getCategoriesFields(cids, fields);
-        });
+    Categories.getAllCategoryFields = async function (fields) {
+        const cids = await Categories.getAllCidsFromSet('categories:cid');
+        return await Categories.getCategoriesFields(cids, fields);
     };
-    Categories.setCategoryField = function (cid, field, value) {
-        return __awaiter(this, void 0, void 0, function* () {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            yield database_1.default.setObjectField(`category:${cid}`, field, value);
-        });
+    Categories.setCategoryField = async function (cid, field, value) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        await database_1.default.setObjectField(`category:${cid}`, field, value);
     };
-    Categories.incrementCategoryFieldBy = function (cid, field, value) {
-        return __awaiter(this, void 0, void 0, function* () {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            yield database_1.default.incrObjectFieldBy(`category:${cid}`, field, value);
-        });
+    Categories.incrementCategoryFieldBy = async function (cid, field, value) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        await database_1.default.incrObjectFieldBy(`category:${cid}`, field, value);
     };
 };
